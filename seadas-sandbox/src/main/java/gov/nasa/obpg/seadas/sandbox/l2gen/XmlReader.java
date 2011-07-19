1998d4c90dd637169fc7bdf39d36be3585e875fb
Dan Knowles <knowles@coral.domain.user>
2011-07-19 14:02:14 -0400

    L2gen gui updates

---
+++ seadas-sandbox/src/main/java/gov/nasa/obpg/seadas/sandbox/l2gen/XmlReader.java
@@ -0,0 +1,14 @@
+package gov.nasa.obpg.seadas.sandbox.l2gen;
+
+/**
+ * A ...
+ *
+ * @author Danny Knowles
+ * @since SeaDAS 7.0
+ */
+public class XmlReader {
+
+    public XmlReader() {
+    }
+
+}
