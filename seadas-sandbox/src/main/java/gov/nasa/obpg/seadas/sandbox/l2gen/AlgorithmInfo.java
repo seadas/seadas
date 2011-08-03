6d64b7fd7cf0665bf7e34335f275c9f533f18086
Danny Knowles <daniel.s.knowles@nasa.gov>
2011-08-03 11:50:50 -0400

    Changed file extension in the output product combobox

--- seadas-sandbox/src/main/java/gov/nasa/obpg/seadas/sandbox/l2gen/AlgorithmInfo.java
+++ seadas-sandbox/src/main/java/gov/nasa/obpg/seadas/sandbox/l2gen/AlgorithmInfo.java
@@ -1,7 +1,7 @@
 package gov.nasa.obpg.seadas.sandbox.l2gen;
 
 /**
- * Need a decent description here...
+ * Need a decent description here, Dude...
  *
  * @author Danny Knowles
  * @since SeaDAS 7.0
