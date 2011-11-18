99d371b8c92b899a319de69e5d028983b3b1477d
Sean Bailey <sean.w.bailey@nasa.gov>
2011-11-18 15:36:30 -0500

    incorporated "seadas mapped" reader into seadas file reader

--- seadas-obpg-reader/src/main/java/gov/nasa/obpg/seadas/dataio/obpg/SeadasMappedFileReader.java
+++ seadas-obpg-reader/src/main/java/gov/nasa/obpg/seadas/dataio/obpg/SeadasMappedFileReader.java
@@ -3,7 +3,12 @@ package gov.nasa.obpg.seadas.dataio.obpg;
 import com.bc.ceres.core.ProgressMonitor;
 import org.esa.beam.framework.dataio.ProductIOException;
 import org.esa.beam.framework.datamodel.*;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.operation.TransformException;
+import ucar.ma2.Array;
 import ucar.nc2.Attribute;
+import ucar.nc2.NetcdfFile;
 
 import java.util.HashMap;
 import java.util.List;
@@ -27,11 +32,86 @@ public class SeadasMappedFileReader extends SeadasFileReader {
 
     @Override
     public Product createProduct() throws ProductIOException {
+        //todo figure out if we even need these...
+        final HashMap<String, String> l2BandInfoMap = getL2BandInfoMap();
+        final HashMap<String, String> l2FlagsInfoMap = getL2FlagsInfoMap();
+        addGlobalAttributeSeadasMapped();
 
+        int sceneWidth = getIntAttribute("Scene Pixels");
+        int sceneHeight = getIntAttribute("Scene Lines");
+        String productName = getStringAttribute("Product Name");
 
-        return null;
+        SeadasProductReader.ProductType productType = productReader.getProductType();
+
+        Product product = new Product(productName, productType.toString(), sceneWidth, sceneHeight);
+        product.setDescription(productName);
+
+        product.setFileLocation(productReader.getInputFile());
+        product.setProductReader(productReader);
+
+        addGlobalMetadata(product);
+        variableMap = addBands(product, ncFile.getVariables(), l2BandInfoMap, l2FlagsInfoMap);
+
+        addGeocoding(product);
+        return product;
 
     }
 
+    public void addGeocoding(Product product) {
+        //todo: figure out how to handle NON-CYLINDRICAL projections...
+        //float pixelX = 0.0f;
+        //float pixelY = 0.0f;
+        // Changed after conversation w/ Sean, Norman F., et al.
+        float pixelX = 0.5f;
+        float pixelY = 0.5f;
+
+        float easting = (float) product.getMetadataRoot().getElement("Global_Attributes").getAttribute("Easternmost Longitude").getData().getElemDouble();
+        float westing = (float) product.getMetadataRoot().getElement("Global_Attributes").getAttribute("Westernmost Longitude").getData().getElemDouble();
+        float pixelSizeX = (easting - westing) / product.getSceneRasterWidth();
+        float northing = (float) product.getMetadataRoot().getElement("Global_Attributes").getAttribute("Northernmost Latitude").getData().getElemDouble();
+        float southing = (float) product.getMetadataRoot().getElement("Global_Attributes").getAttribute("Southernmost Latitude").getData().getElemDouble();
+        float pixelSizeY = (northing - southing) / product.getSceneRasterHeight();
+
+        try {
+            product.setGeoCoding(new CrsGeoCoding(DefaultGeographicCRS.WGS84,
+                    product.getSceneRasterWidth(),
+                    product.getSceneRasterHeight(),
+                    westing, northing,
+                    pixelSizeX, pixelSizeY,
+                    pixelX, pixelY));
+        } catch (FactoryException e) {
+            throw new IllegalStateException(e);
+        } catch (TransformException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public void addGlobalAttributeSeadasMapped(){
+        int [] dims = ncFile.getVariables().get(0).getShape();
+        String [] prodname = ncFile.getLocation().split("/");
+        String projname = ncFile.getVariables().get(0).findAttribute("Projection Name").getStringValue();
+        Array projlimits = ncFile.getVariables().get(0).findAttribute("Limit").getValues();
+        double north = projlimits.getDouble(2);
+        double south = projlimits.getDouble(0);
+        double east = projlimits.getDouble(3);
+        double west = projlimits.getDouble(1);
+
+        Attribute rasterWidth = new Attribute("Scene Pixels",dims[1]);
+        Attribute rasterHeight = new Attribute("Scene Lines",dims[0]);
+        Attribute productName = new Attribute("Product Name",prodname[prodname.length-1]);
+        Attribute projection = new Attribute("Projection Name",projname);
+        Attribute northing = new Attribute("Northernmost Latitude",north);
+        Attribute southing = new Attribute("Southernmost Latitude",south);
+        Attribute easting = new Attribute("Easternmost Longitude",east);
+        Attribute westing = new Attribute("Westernmost Longitude",west);
+        globalAttributes.add(rasterHeight);
+        globalAttributes.add(rasterWidth);
+        globalAttributes.add(productName);
+        globalAttributes.add(projection);
+        globalAttributes.add(northing);
+        globalAttributes.add(southing);
+        globalAttributes.add(easting);
+        globalAttributes.add(westing);
+    }
 
 }
