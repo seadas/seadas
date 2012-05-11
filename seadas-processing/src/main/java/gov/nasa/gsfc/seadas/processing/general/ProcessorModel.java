23f33331d294d3d53d2285cf4f586058a66daebe
Don Shea <don.shea@nasa.gov>
2012-05-11 15:00:16 -0400

    updated the paramInfo XML file again

--- seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/general/ProcessorModel.java
+++ seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/general/ProcessorModel.java
@@ -389,7 +389,6 @@ public class ProcessorModel {
         }
     }
 
-
     public void disableEvent(String name) {
         EventInfo eventInfo = getEventInfo(name);
         if (eventInfo == null) {
