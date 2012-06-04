52dffe4bf74eab56cee52307f36c074713d433e7
Aynur Abdurazik <aynur.abdurazik@gmail.com>
2012-06-04 17:10:50 -0400

    Cleaned up the source code.

--- seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/general/ExtractorUI.java
+++ seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/general/ExtractorUI.java
@@ -33,19 +33,7 @@ public class ExtractorUI extends CloProgramUIImpl {
     public ExtractorUI(String programName, String xmlFileName) {
         super(programName, xmlFileName);
 
-        //lonlat2pixline.hasOutputFile(false);
-
-//        sourceProductSelector = new SourceProductFileSelector(VisatApp.getApp(), "");
-//        //sourceProductSelector.setProcessorModel(extractor);
-//        sourceProductSelector.initProducts();
-//
-//        outputFileSelector = new OutputFileSelector(VisatApp.getApp(), "Output File");
-//        this.setName("mainPanel");
-//        initUI();
     }
-
-
-    //private void
 
     private File getGeoFileName(File inputFile) {
 
@@ -55,8 +43,6 @@ public class ExtractorUI extends CloProgramUIImpl {
         }
         geoFileName = geoFileName.substring(0, geoFileName.indexOf("."));
         geoFileName = geoFileName.concat(".GEO");
-
-        //System.out.println("geofileName = " + geoFileName);
         return new File(inputFile.getParentFile(), geoFileName);
     }
 
@@ -65,13 +51,10 @@ public class ExtractorUI extends CloProgramUIImpl {
         Product selectedProduct = sourceProductSelector.getSelectedProduct();
         if (sourceProductSelector.getSelectedProduct() != null) {
             final File inputFile = selectedProduct.getFileLocation();
-            //System.out.println("update processors model " + inputFile.toString());
             extractor.updateParamInfo(extractor.getPrimaryInputFileOptionName(), inputFile.toString());
-
-            //lonlat2pixline.setInputFile(getGeoFileName(inputFile));
             lonlat2pixline.updateParamInfo(lonlat2pixline.getPrimaryInputFileOptionName(), getGeoFileName(inputFile).toString());
         }
-            extractor.updateParamInfo(extractor.getPrimaryOutputFileOptionName(), outputFileSelector.getFileName());
+        extractor.updateParamInfo(extractor.getPrimaryOutputFileOptionName(), outputFileSelector.getFileName());
 
     }
 
@@ -115,8 +98,6 @@ public class ExtractorUI extends CloProgramUIImpl {
                 SeadasLogger.getLogger().finest(line);
                 if (line.indexOf("=") != -1) {
                     tmp = line.split("=");
-
-                    //System.out.printf("Option name: %1$s  Value: %2$s %n", tmp[0], tmp[1]);
                     extractor.updateParamInfo(tmp[0], tmp[1]);
                 }
             }
@@ -138,7 +119,6 @@ public class ExtractorUI extends CloProgramUIImpl {
         newsPanel.addPropertyChangeListener(new PropertyChangeListener() {
             @Override
             public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
-                //System.out.println(propertyChangeEvent.getPropertyName());
             }
         });
         newsPanel.getComponent(0);
@@ -156,7 +136,6 @@ public class ExtractorUI extends CloProgramUIImpl {
 
                 if (pixellonlatSwitch.isSelected()) {
                     pixellonlatSwitch.setBorderPainted(true);
-                    //System.out.println(pixellonlatSwitch.isSelected());
                     updateProcessorModel();
                     computePixelsFromLonLat();
                     pixelPanel = createParamPanel(extractor);
@@ -168,141 +147,24 @@ public class ExtractorUI extends CloProgramUIImpl {
         });
 
         paramPanel = new JPanel(new GridBagLayout());
-        //TableLayout paramPanelLayout = new TableLayout(2);
-        //paramPanel.setLayout(paramPanelLayout);
-
-//        paramPanel.add(newsPanel, new GridBagConstraintsCustom(0, 0, 3, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH));
-//        paramPanel.add(pixelPanel , new GridBagConstraintsCustom(1, 0, 3, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH));
-//        paramPanel.add(pixellonlatSwitch, new GridBagConstraintsCustom(3, 0, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE));
-        //paramPanelLayout.setCellRowspan(1, 1, 2);
-        paramPanel.add(newsPanel,
+         paramPanel.add(newsPanel,
                 new GridBagConstraintsCustom(0, 0, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, 2));
         paramPanel.add(pixellonlatSwitch,
                 new GridBagConstraintsCustom(1, 0, 1, 0, GridBagConstraints.EAST, GridBagConstraints.NONE, 2));
         paramPanel.add(pixelPanel,
                 new GridBagConstraintsCustom(0, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, 2));
-//        mainPanel.add(createFilterPane(),
-//                new GridBagConstraintsCustom(3, 0, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, 2));
-        //paramPanel.add(newsPanel);
-        //paramPanel.add(pixellonlatSwitch);
-        //paramPanel.add(pixelPanel);
-
         return paramPanel;
     }
 
     private void updateParamPanel() {
 
-        //System.out.println(pixellonlatSwitch.isSelected());
-       // if (pixellonlatSwitch.isSelected()) {
-
-            paramPanel.remove(paramPanel.getComponent(2));
-            paramPanel.add(pixelPanel, new GridBagConstraintsCustom(0, 1, 3, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH));
-            pixellonlatSwitch.setEnabled(false);
+        paramPanel.remove(paramPanel.getComponent(2));
+        paramPanel.add(pixelPanel, new GridBagConstraintsCustom(0, 1, 3, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH));
+        pixellonlatSwitch.setEnabled(false);
         pixellonlatSwitch.setBorderPainted(false);
-//        } else {
-//            paramPanel.remove(pixelPanel);
-//            paramPanel.add(newsPanel, new GridBagConstraintsCustom(0, 0, 3, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH));
-//        }
-        // System.out.println("component 0: " + paramPanel.getComponent(0).getName());
-        //System.out.println("component 1: " +paramPanel.getComponent(1).getName());
-        //System.out.println("component 2: " +paramPanel.getComponent(2).getName());
         paramPanel.validate();
         paramPanel.repaint(50L);
     }
-
-//    private JPanel createParamPanel(ArrayList<ParamInfo> paramList) {
-//
-//        JPanel paramPanel = new JPanel();
-//        Dimension paramPanelDimension = new Dimension(100, 100);
-//        TableLayout lonlatLayout = new TableLayout(2);
-//        paramPanel.setLayout(lonlatLayout);
-//        paramPanel.setPreferredSize(paramPanelDimension);
-//
-//        Iterator itr = paramList.iterator();
-//        while (itr.hasNext()) {
-//            final ParamInfo pi = (ParamInfo) itr.next();
-//            if (!(pi.getName().equals(ParamUtils.IFILE) || pi.getName().equals("infile") || pi.getName().equals(ParamUtils.OFILE))) {
-//                paramPanel.add(makeOptionField(pi));
-//            }
-//        }
-//
-//        paramPanel.addMouseListener(new MouseListener() {
-//            @Override
-//            public void mouseClicked(MouseEvent mouseEvent) {
-//                //To change body of implemented methods use File | Settings | File Templates.
-//            }
-//
-//            @Override
-//            public void mousePressed(MouseEvent mouseEvent) {
-//                //To change body of implemented methods use File | Settings | File Templates.
-//            }
-//
-//            @Override
-//            public void mouseReleased(MouseEvent mouseEvent) {
-//                //To change body of implemented methods use File | Settings | File Templates.
-//            }
-//
-//            @Override
-//            public void mouseEntered(MouseEvent mouseEvent) {
-//                //To change body of implemented methods use File | Settings | File Templates.
-//            }
-//
-//            @Override
-//            public void mouseExited(MouseEvent mouseEvent) {
-//                //To change body of implemented methods use File | Settings | File Templates.
-//
-//                validateParams();
-//            }
-//        });
-//        return paramPanel;
-//    }
-//
-//    private JPanel makeOptionField(final ParamInfo pi) {
-//
-//        final String optionName = pi.getName();
-//        final String optionValue = pi.getValue();
-//        final JPanel optionPanel = new JPanel();
-//        optionPanel.setLayout(new BoxLayout(optionPanel, BoxLayout.Y_AXIS));
-//        optionPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-//        optionPanel.add(new JLabel(optionName));
-//
-//
-//        final PropertyContainer vc = new PropertyContainer();
-//        vc.addProperty(Property.create(optionName, optionValue));
-//        vc.getDescriptor(optionName).setDisplayName(optionName);
-//
-////        final ValueRange valueRange = new ValueRange(-180, 180);
-////
-////
-////        vc.getDescriptor(optionName).setValueRange(valueRange);
-//
-//        final BindingContext ctx = new BindingContext(vc);
-//        final JTextField field = new JTextField();
-//        field.setColumns(8);
-//        field.setHorizontalAlignment(JFormattedTextField.LEFT);
-//        System.out.println(optionName + "  " + optionValue);
-//        ctx.bind(optionName, field);
-//
-//        ctx.addPropertyChangeListener(optionName, new PropertyChangeListener() {
-//
-//            @Override
-//            public void propertyChange(PropertyChangeEvent pce) {
-//                pi.setValue(field.getText());
-//
-//            }
-//        });
-//
-//        optionPanel.add(field);
-//
-//        return optionPanel;
-//
-//    }
-
-    private void validateParams() {
-
-    }
-
-    //int lonLatChanges = 0;
 
     protected void handleParamChanged() {
         if (lonlat2pixline.isAllParamsValid()) {
