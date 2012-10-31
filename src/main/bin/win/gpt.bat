438dcdd3269364b6377983fa2af2f21091fbe941
Sean Bailey <sean.w.bailey@nasa.gov>
2012-10-31 13:17:15 -0400

    fixed issues as noted on oceancolor forum: http://oceancolor.gsfc.nasa.gov/forum/oceancolor/topic_show.pl?pid=20161#pid20161

--- src/main/bin/win/gpt.bat
+++ src/main/bin/win/gpt.bat
@@ -2,10 +2,10 @@
 
 set SEADAS_HOME=${installer:sys.installationDir}
 
-"%SEADAS_HOME%\jre\bin\java.exe" ^
+"%JAVA_HOME%\jre\bin\java.exe" ^
     -Xmx1024M ^
     -Dceres.context=seadas ^
-    "-Dbeam.mainClass=org.esa.beam.framework.gpf.main.Main" ^
+    "-Dbeam.mainClass=org.esa.beam.framework.gpf.main.GPT" ^
     "-Dbeam.home=%SEADAS_HOME%" ^
     "-Dncsa.hdf.hdflib.HDFLibrary.hdflib=%SEADAS_HOME%\modules\lib-hdf-${hdf.version}\lib\jhdf.dll" ^
     "-Dncsa.hdf.hdf5lib.H5.hdf5lib=%SEADAS_HOME%\modules\lib-hdf-${hdf.version}\lib\jhdf5.dll" ^
