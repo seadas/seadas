ba4defd158dbdd91ac6b3b69afd2251365cd4e17
Danny Knowles <daniel.s.knowles@nasa.gov>
2014-03-07 14:57:34 -0500

    Merge branch 'master' of /disk01/SeaDAS/seadas with conflicts.

--- seadas-app/src/main/java/gov/nasa/gsfc/seadas/SeadasApp.java
+++ seadas-app/src/main/java/gov/nasa/gsfc/seadas/SeadasApp.java
@@ -15,8 +15,29 @@
  */
 package gov.nasa.gsfc.seadas;
 
+import com.jidesoft.action.CommandBar;
+import org.esa.beam.framework.datamodel.PlacemarkDescriptor;
+import org.esa.beam.framework.datamodel.PlacemarkDescriptorRegistry;
 import org.esa.beam.framework.ui.application.ApplicationDescriptor;
+import org.esa.beam.framework.ui.application.ToolViewDescriptor;
+import org.esa.beam.visat.ProductsToolView;
+import org.esa.beam.visat.VisatActivator;
 import org.esa.beam.visat.VisatApp;
+import org.esa.beam.visat.actions.CreateSubsetFromViewAction;
+import org.esa.beam.visat.actions.ShowToolBarAction;
+import org.esa.beam.visat.toolviews.diag.TileCacheDiagnosisToolView;
+import org.esa.beam.visat.toolviews.imageinfo.ColorManipulationToolView;
+import org.esa.beam.visat.toolviews.layermanager.LayerManagerToolView;
+import org.esa.beam.visat.toolviews.mask.MaskManagerToolView;
+import org.esa.beam.visat.toolviews.nav.NavigationToolView;
+import org.esa.beam.visat.toolviews.pixelinfo.PixelInfoToolView;
+import org.esa.beam.visat.toolviews.placemark.PlacemarkEditorToolView;
+import org.esa.beam.visat.toolviews.placemark.gcp.GcpManagerToolView;
+import org.esa.beam.visat.toolviews.spectrum.SpectrumToolView;
+import org.esa.beam.visat.toolviews.stat.*;
+
+import javax.swing.*;
+import java.util.*;
 
 /**
  * The <code>SeadasApp</code> class represents the SeaDAS UI application.
@@ -24,6 +45,8 @@ import org.esa.beam.visat.VisatApp;
  * @author Don
  */
 public class SeadasApp extends VisatApp {
+
+    private static final String SHOW_TOOLVIEW_CMD_POSTFIX = ".showCmd";
 
     /**
      * Constructs the SeaDAS UI application instance. The constructor does not start the application nor does it perform any GUI
@@ -36,4 +59,131 @@ public class SeadasApp extends VisatApp {
         // SystemUtils.BEAM_HOME_PAGE = "http://seadas.gsfc.nasa.gov/";
     }
 
+    /**
+     * Overrides the base class version in order to create a tool bar for VISAT.
+     */
+    @Override
+    protected CommandBar createMainToolBar() {
+        final CommandBar toolBar = createToolBar(MAIN_TOOL_BAR_ID, "Standard");
+        addCommandsToToolBar(toolBar, new String[]{
+                "new",
+                "open",
+                null,
+                "collocation",
+                "createSubsetFromView",
+                "bandArithmetic"
+        });
+        return toolBar;
+    }
+
+
+    @Override
+    protected CommandBar createLayersToolBar() {
+        final CommandBar toolBar = createToolBar(LAYERS_TOOL_BAR_ID, "Layers");
+        ArrayList<String> commandIdList = new ArrayList<String>(Arrays.asList(
+                "showNoDataOverlay",
+                "showShapeOverlay",
+                "showGraticuleOverlay",
+                "showWorldMapOverlay",
+                "collocation",
+                "createSubsetFromView",
+                "bandArithmetic"));
+        Set<PlacemarkDescriptor> placemarkDescriptors = PlacemarkDescriptorRegistry.getInstance().getPlacemarkDescriptors();
+        for (PlacemarkDescriptor placemarkDescriptor : placemarkDescriptors) {
+            if (placemarkDescriptor.getShowLayerCommandId() != null) {
+                if (!placemarkDescriptor.getBaseFeatureType().getName().equals("http://www.opengiv.net/gml:org.esa.beam.GroundControlPoint")) {
+                    commandIdList.add(placemarkDescriptor.getShowLayerCommandId());
+                }
+            }
+        }
+        addCommandsToToolBar(toolBar, commandIdList.toArray(new String[0]));
+        return toolBar;
+    }
+
+    @Override
+    protected CommandBar createAnalysisToolBar() {
+        final CommandBar toolBar = createToolBar(ANALYSIS_TOOL_BAR_ID, "Analysis");
+        addCommandsToToolBar(toolBar, new String[]{
+                StatisticsToolView.ID + SHOW_TOOLVIEW_CMD_POSTFIX,
+                HistogramPlotToolView.ID + SHOW_TOOLVIEW_CMD_POSTFIX,
+                DensityPlotToolView.ID + SHOW_TOOLVIEW_CMD_POSTFIX,
+                ScatterPlotToolView.ID + SHOW_TOOLVIEW_CMD_POSTFIX,
+                ProfilePlotToolView.ID + SHOW_TOOLVIEW_CMD_POSTFIX
+        });
+        return toolBar;
+    }
+
+
+
+    @Override
+    protected CommandBar[] createViewsToolBars() {
+
+        final HashSet<String> excludedIds = new HashSet<String>(14);
+        // todo - remove bad forward dependencies to tool views (nf - 30.10.2008)
+        excludedIds.add(TileCacheDiagnosisToolView.ID);
+        excludedIds.add(ProductsToolView.ID);
+        excludedIds.add(ColorManipulationToolView.ID);
+        excludedIds.add(NavigationToolView.ID);
+        excludedIds.add(MaskManagerToolView.ID);
+        excludedIds.add(GcpManagerToolView.ID);
+//        excludedIds.add(LayerManagerToolView.ID);
+        excludedIds.add(PixelInfoToolView.ID);
+        excludedIds.add(PlacemarkEditorToolView.ID);
+        excludedIds.add(InformationToolView.ID);
+        excludedIds.add(GeoCodingToolView.ID);
+        excludedIds.add(StatisticsToolView.ID);
+        excludedIds.add(HistogramPlotToolView.ID);
+        excludedIds.add(ScatterPlotToolView.ID);
+        excludedIds.add(DensityPlotToolView.ID);
+        excludedIds.add(ProfilePlotToolView.ID);
+        excludedIds.add("org.esa.beam.scripting.visat.ScriptConsoleToolView");
+        excludedIds.add("org.esa.beam.visat.toolviews.layermanager.LayerEditorToolView");
+
+
+        ToolViewDescriptor[] toolViewDescriptors = VisatActivator.getInstance().getToolViewDescriptors();
+
+        Map<String, List<String>> toolBar2commandIds = new HashMap<String, List<String>>();
+        for (ToolViewDescriptor toolViewDescriptor : toolViewDescriptors) {
+            if (!excludedIds.contains(toolViewDescriptor.getId())) {
+                final String commandId = toolViewDescriptor.getId() + ".showCmd";
+
+                String toolBarId = toolViewDescriptor.getToolBarId();
+                if (toolBarId == null || toolBarId.isEmpty()) {
+                    toolBarId = VIEWS_TOOL_BAR_ID;
+                }
+
+                List<String> commandIds = toolBar2commandIds.get(toolBarId);
+                if (commandIds == null) {
+                    commandIds = new ArrayList<String>(5);
+                    toolBar2commandIds.put(toolBarId, commandIds);
+                }
+                commandIds.add(commandId);
+            }
+        }
+
+        List<CommandBar> viewToolBars = new ArrayList<CommandBar>(5);
+        viewToolBars.add(createToolBar(VIEWS_TOOL_BAR_ID, "Views"));
+        for (String toolBarId : toolBar2commandIds.keySet()) {
+            CommandBar toolBar = getToolBar(toolBarId);
+            if (toolBar == null) {
+                // todo - use ToolBarDescriptor to define tool bar properties, e.g. title, dockSite, ...  (nf - 20090119)
+                toolBar = createToolBar(toolBarId, toolBarId.replace('.', ' ').replace('_', ' '));
+                viewToolBars.add(toolBar);
+
+                // 	Retrospectively add "tool bar toggle" menu item
+                ShowToolBarAction action = new ShowToolBarAction(toolBarId + ".showToolBar");
+                action.setText(toolBarId);
+                action.setContexts(new String[]{toolBarId});
+                action.setToggle(true);
+                action.setSelected(true);
+                getCommandManager().addCommand(action);
+                JMenu toolBarsMenu = findMenu("toolBars");
+                toolBarsMenu.add(action.createMenuItem());
+            }
+            List<String> commandIds = toolBar2commandIds.get(toolBarId);
+            addCommandsToToolBar(toolBar, commandIds.toArray(new String[commandIds.size()]));
+        }
+
+        return viewToolBars.toArray(new CommandBar[viewToolBars.size()]);
+    }
 }
--- seadas-contour-operator/src/main/java/gov/nasa/gsfc/seadas/contour/ui/ContourVPI.java
+++ seadas-contour-operator/src/main/java/gov/nasa/gsfc/seadas/contour/ui/ContourVPI.java
@@ -2,23 +2,21 @@ package gov.nasa.gsfc.seadas.contour.ui;
 
 import com.bc.ceres.glevel.MultiLevelImage;
 import com.jidesoft.action.CommandBar;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.LineString;
-import gov.nasa.gsfc.seadas.ContourDescriptor;
 import gov.nasa.gsfc.seadas.contour.util.ResourceInstallationUtils;
 import org.esa.beam.framework.datamodel.Band;
-import org.esa.beam.framework.datamodel.Mask;
 import org.esa.beam.framework.datamodel.Product;
-import org.esa.beam.framework.datamodel.ProductNodeGroup;
 import org.esa.beam.framework.ui.command.CommandAdapter;
 import org.esa.beam.framework.ui.command.CommandEvent;
 import org.esa.beam.framework.ui.command.ExecCommand;
 import org.esa.beam.visat.AbstractVisatPlugIn;
 import org.esa.beam.visat.VisatApp;
-import org.jaitools.swing.ImageFrame;
-import org.jaitools.swing.JTSFrame;
+// import org.jaitools.media.jai.contour.ContourDescriptor;
+import org.opengis.geometry.coordinate.LineString;
 
-import javax.media.jai.*;
+import javax.media.jai.ImageLayout;
+import javax.media.jai.JAI;
+import javax.media.jai.ParameterBlockJAI;
+import javax.media.jai.RenderedOp;
 import javax.media.jai.operator.FormatDescriptor;
 import javax.swing.*;
 import java.awt.*;
@@ -97,19 +95,157 @@ public class ContourVPI extends AbstractVisatPlugIn {
     private void showContour(final VisatApp visatApp) {
 
         final Product product = visatApp.getSelectedProduct();
-        Band band = product.getBand("MyAverageBand");
-
-
-        if (product != null) {
-            final ProductNodeGroup<Mask> maskGroup = product.getMaskGroup();
-            final ProductNodeGroup<Band> bandGroup = product.getBandGroup();
-
-
-            ContourDialog contourDialog = new ContourDialog(product);
-                                contourDialog.setVisible(true);
-                                contourDialog.dispose();
-            band.setSourceImage(getContourImage(product, band));
-         }
+        Band band = product.getBand("chlor_a");
+        band.setSourceImage(getContourImage(product, band));
+//
+//        if (product != null) {
+//            final ProductNodeGroup<Mask> maskGroup = product.getMaskGroup();
+//            final ProductNodeGroup<Band> bandGroup = product.getBandGroup();
+//
+//            /*
+//               A simple boolean switch to enable this to run with or without the intermediate user dialogs
+//            */
+//            boolean useDialogs = true;
+//
+//            final ContourData contourData = new ContourData();
+//
+//
+//            if (!useDialogs) {
+//                contourData.setCreateMasks(true);
+//            }
+//
+//
+//            /*
+//                Determine whether these auxilliary masks and associated products have already be created.
+//                This would be the case when run a second time on the same product.
+//            */
+//
+//            final boolean[] masksCreated = {false};
+//
+//            for (String name : maskGroup.getNodeNames()) {
+//                if (name.equals(contourData.getMaskName())) {
+//                    masksCreated[0] = true;
+//                }
+//            }
+//
+//
+//            for (String name : bandGroup.getNodeNames()) {
+//                if (name.equals(contourData.getContourBandName())) {
+//                    masksCreated[0] = true;
+//                }
+//            }
+//
+//            /*
+//               For the case where this is being run a second time, prompt the user to determine whether to delete
+//               and re-create the products and masks.
+//            */
+//
+//            if (masksCreated[0]) {
+//                if (useDialogs) {
+//                    contourData.setDeleteMasks(false);
+//                    ContourDialog contourDialog = new ContourDialog(contourData, masksCreated[0], product);
+//                    contourDialog.setVisible(true);
+//                    contourDialog.dispose();
+//                }
+//
+//                if (contourData.isDeleteMasks() || !useDialogs) {
+//                    masksCreated[0] = false;
+//
+//                    for (String name : maskGroup.getNodeNames()) {
+//                        if (name.equals(contourData.getMaskName())) {
+//                            maskGroup.remove(maskGroup.get(name));
+//                        }
+//                    }
+//                    for (String name : bandGroup.getNodeNames()) {
+//                        if (name.equals(contourData.getContourBandName())) {
+//                            bandGroup.remove(bandGroup.get(name));
+//                        }
+//                    }
+//                }
+//            }
+//
+//
+//            if (!masksCreated[0]) {
+//                if (useDialogs) {
+//                    contourData.setCreateMasks(false);
+//                    ContourDialog contourDialog = new ContourDialog(contourData, masksCreated[0], product);
+//                    contourDialog.setVisible(true);
+//                }
+//
+//                if (contourData.isCreateMasks()) {
+//                    final SourceFileInfo sourceFileInfo = contourData.getSourceFileInfo();
+//
+//                    if (sourceFileInfo.isEnabled()) {
+//
+//                        ProgressMonitorSwingWorker pmSwingWorker = new ProgressMonitorSwingWorker(visatApp.getMainFrame(),
+//                                "Creating contour band and mask") {
+//
+//                            @Override
+//                            protected Void doInBackground(com.bc.ceres.core.ProgressMonitor pm) throws Exception {
+//
+//                                pm.beginTask("Creating contour band and mask", 2);
+//
+//                                try {
+//                                    Map<String, Object> parameters = new HashMap<String, Object>();
+//
+//                                    parameters.put("subSamplingFactorX", new Integer(contourData.getSuperSampling()));
+//                                    parameters.put("subSamplingFactorY", new Integer(contourData.getSuperSampling()));
+//                                    parameters.put("resolution", sourceFileInfo.getResolution(SourceFileInfo.Unit.METER));
+//                                    parameters.put("filename", sourceFileInfo.getFile().getName());
+//
+//                                    /*
+//                                       Create a new product, which will contain the contour band, then add this band to current product.
+//                                    */
+//
+//                                    Product contourProduct = GPF.createProduct(CONTOUR_PRODUCT_NAME, parameters, product);
+//                                    Band contourBand = contourProduct.getBand(ContourOp.CONTOUR_BAND_NAME);
+//                                    reformatSourceImage(contourBand, new ImageLayout(product.getBandAt(0).getSourceImage()));
+//                                    pm.worked(1);
+//                                    contourBand.setName(contourData.getContourBandName());
+//
+//                                    product.addBand(contourBand);
+//
+//                                    String maskMath = contourData.getMaskMath();
+//
+//                                    Mask contourMask = Mask.BandMathsType.create(
+//                                            contourData.getMaskName(),
+//                                            contourData.getMaskDescription(),
+//                                            product.getSceneRasterWidth(),
+//                                            product.getSceneRasterHeight(),
+//                                            contourData.getMaskMath(),
+//                                            contourData.getMaskColor(),
+//                                            contourData.getMaskTransparency());
+//                                    maskGroup.add(contourMask);
+//
+//                                    pm.worked(1);
+//
+//                                    String[] bandNames = product.getBandNames();
+//                                    for (String bandName : bandNames) {
+//                                        RasterDataNode raster = product.getRasterDataNode(bandName);
+//                                        if (contourData.isShowMaskAllBands()) {
+//                                            raster.getOverlayMaskGroup().add(contourMask);
+//                                        }
+//                                    }
+//
+//
+//                                } finally {
+//                                    pm.done();
+//                                }
+//                                return null;
+//                            }
+//                        };
+//
+//                        pmSwingWorker.executeWithBlocking();
+//
+//                    } else {
+//                        SimpleDialogMessage dialog = new SimpleDialogMessage(null, "Cannot Create Masks: Resolution File Doesn't Exist");
+//                        dialog.setVisible(true);
+//                        dialog.setEnabled(true);
+//
+//                    }
+//                }
+//            }
+//        }
     }
 
 
@@ -126,45 +262,29 @@ public class ContourVPI extends AbstractVisatPlugIn {
         JAI.setDefaultTileSize(new Dimension(512, 512));
         ArrayList<Double> contourIntervals = new ArrayList<Double>();
 
-        for (double level = 1; level < 10; level += 2) {
+        for (double level = 0.2; level < 1.41; level += 0.2) {
             contourIntervals.add(level);
         }
-
-        //Contour1Spi cspi = new Contour1Spi();
-
-        //cspi.updateRegistry(JAI.getDefaultInstance().getOperationRegistry());
-
-        OperationRegistry or = JAI.getDefaultInstance().getOperationRegistry();
-            String modeName = "rendered";
-            String[] descriptorNames;
-
-            for (String name : or.getDescriptorNames(modeName)) {
-                System.out.println(name);
-            }
-        Band band1 = product.getBand("MyAverageBand");
-        PlanarImage pi = product.getBand("MyAverageBand").getSourceImage();
         ParameterBlockJAI pb = new ParameterBlockJAI("Contour");
-        pb.setSource("source0", band1.getGeophysicalImage());
+        pb.setSource("source0", band.getSourceImage());
         pb.setParameter("levels", contourIntervals);
         RenderedOp dest = JAI.create("Contour", pb);
-        Collection<LineString> contours = (Collection<LineString>) dest.getProperty(ContourDescriptor.CONTOUR_PROPERTY_NAME);
-
-
-        JTSFrame jtsFrame = new JTSFrame("Contours from source image");
-        for (LineString contour : contours) {
-            jtsFrame.addGeometry((Geometry) contour, Color.BLUE);
-        }
-
-        ImageFrame imgFrame = new ImageFrame(dest.getRendering(), "Source image");
-        imgFrame.setLocation(100, 100);
-        imgFrame.setVisible(true);
-
-        Dimension size = imgFrame.getSize();
-        jtsFrame.setSize(size);
-        jtsFrame.setLocation(100 + size.width + 5, 100);
-        jtsFrame.setVisible(true);
-
+      //  Collection<LineString> contours = (Collection<LineString>) dest.getProperty(ContourDescriptor.CONTOUR_PROPERTY_NAME);
         return (MultiLevelImage) dest.getRendering();
+
+//        JTSFrame jtsFrame = new JTSFrame("Contours from source image");
+//        for (LineString contour : contours) {
+//            jtsFrame.addGeometry(contour, Color.BLUE);
+//        }
+//
+//        ImageFrame imgFrame = new ImageFrame(image, "Source image");
+//        imgFrame.setLocation(100, 100);
+//        imgFrame.setVisible(true);
+//
+//        Dimension size = imgFrame.getSize();
+//        jtsFrame.setSize(size);
+//        jtsFrame.setLocation(100 + size.width + 5, 100);
+//        jtsFrame.setVisible(true);
 
     }
 
