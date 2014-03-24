5fd317f7770442964ec1a015ea585e0b18e110ed
Aynur Abdurazik <aynur.abdurazik@nasa.gov>
2014-03-24 10:39:14 -0400

    committing changes before merging with master.

---
+++ seadas-contour-operator/src/main/java/gov/nasa/gsfc/seadas/contour/ui/ContourRendering.java
@@ -0,0 +1,11 @@
+package gov.nasa.gsfc.seadas.contour.ui;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Aynur Abdurazik (aabduraz)
+ * Date: 3/6/14
+ * Time: 12:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ContourRendering {
+}
---
+++ seadas-contour-operator/src/main/java/gov/nasa/gsfc/seadas/contour/ui/InsertContourLineFigureInteractor.java
@@ -0,0 +1,11 @@
+package gov.nasa.gsfc.seadas.contour.ui;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Aynur Abdurazik (aabduraz)
+ * Date: 3/14/14
+ * Time: 3:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class InsertContourLineFigureInteractor {
+}
