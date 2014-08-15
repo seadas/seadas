b9f45a33138237e50f8e65b4727463ab533a4330
Danny Knowles <daniel.s.knowles@nasa.gov>
2014-08-15 12:46:37 -0400

    Bathymetry: changed band names to elevation_bathymetry and elevation_topography.  Added color scheme to the defaults, though for some reason it does not seem to be used at the moment when loaded as the default.

--- seadas-bathymetry-operator/src/main/java/gov/nasa/gsfc/seadas/bathymetry/operator/BathymetryOp.java
+++ seadas-bathymetry-operator/src/main/java/gov/nasa/gsfc/seadas/bathymetry/operator/BathymetryOp.java
@@ -43,9 +43,9 @@ import java.text.MessageFormat;
         description = "Operator creating a bathymetry band, elevation band, topography band and bathymetry mask")
 public class BathymetryOp extends Operator {
 
-    public static final String BATHYMETRY_BAND_NAME = "bathymetryBand";
+    public static final String BATHYMETRY_BAND_NAME = "elevation_bathymetry";
     public static final String ELEVATION_BAND_NAME = "elevation";
-    public static final String TOPOGRAPHY_BAND_NAME = "topography";
+    public static final String TOPOGRAPHY_BAND_NAME = "elevation_topography";
 
     @SourceProduct(alias = "source", description = "The Product the land/water-mask shall be computed for.",
             label = "Name")
