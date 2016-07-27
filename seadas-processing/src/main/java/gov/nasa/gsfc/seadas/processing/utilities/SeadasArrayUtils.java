05529bdb5a9ed2b42546c8fae41d1096939886f6
Aynur Abdurazik <aynur.abdurazik@nasa.gov>
2016-07-27 18:09:20 -0400

    Added a SeadasArrayUtils.java to concatinate arrays in general. It's used to concatinate command array parts.

---
+++ seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/utilities/SeadasArrayUtils.java
@@ -0,0 +1,43 @@
+package gov.nasa.gsfc.seadas.processing.utilities;
+
+import java.util.Arrays;
+
+/**
+ * Created by aabduraz on 7/25/16.
+ */
+public class SeadasArrayUtils {
+    /**
+     * Concatenating two arrays
+     * @param first First array to be concatenated
+     * @param second Second array to be concatenated
+     * @param <T>
+     * @return
+     */
+    public static <T> T[] concat(T[] first, T[] second) {
+        T[] result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
+    }
+
+    /**
+     *Concatenating an arbitrary number of arrays
+     * @param first First array in the list of arrays
+     * @param rest Rest of the arrays in the list to be concatenated
+     * @param <T>
+     * @return
+     */
+    public static <T> T[] concatAll(T[] first, T[]... rest) {
+        int totalLength = first.length;
+        for (T[] array : rest) {
+            totalLength += array.length;
+        }
+        T[] result = Arrays.copyOf(first, totalLength);
+        int offset = first.length;
+        for (T[] array : rest) {
+            System.arraycopy(array, 0, result, offset, array.length);
+            offset += array.length;
+        }
+        return result;
+    }
+
+}
