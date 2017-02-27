b505fcbfc0f1cf382be873e537c940dff0fe13e6
aynur <aynur.abdurazik@nasa.gov>
2017-02-27 16:18:53 -0500

    Reorganizing server side code.

--- seadas-ocsswrest/src/main/java/gov/nasa/gsfc/seadas/ocsswrest/utilities/OCSSWInfo.java
+++ seadas-ocsswrest/src/main/java/gov/nasa/gsfc/seadas/ocsswrest/utilities/OCSSWInfo.java
@@ -1,6 +1,7 @@
 package gov.nasa.gsfc.seadas.ocsswrest.utilities;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import java.io.File;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,6 +12,14 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement
 public class OCSSWInfo {
+    public static final String _OCSSWROOT_ENVVAR = "OCSSWROOT";
+    public static String OCSSW_INSTALLER = "install_ocssw.py";
+    public static String OCSSW_RUNNER = "ocssw_runner";
+    public static String TMP_OCSSW_INSTALLER = (new File(System.getProperty("java.io.tmpdir"), "install_ocssw.py")).getPath();
+    public static String OCSSW_INSTALLER_URL = "https://oceandata.sci.gsfc.nasa.gov/ocssw/install_ocssw.py";
+
+    public static String _OCSSW_SCRIPTS_DIR_SUFFIX =  System.getProperty("file.separator") + "run" +  System.getProperty("file.separator") + "scripts";
+    public static String _OCSSW_DATA_DIR_SUFFIX =  System.getProperty("file.separator") + "run" +  System.getProperty("file.separator") + "data";
     private boolean installed;
     private String ocsswDir;
 
@@ -22,12 +31,17 @@ public class OCSSWInfo {
         this.installed = installed;
     }
 
-    public String getOcsswDir(){
+    public String getOcsswInstallDir(){
+        ocsswDir = System.getenv(_OCSSWROOT_ENVVAR);
         return ocsswDir;
     }
 
-    public void setOcsswDir(String ocsswDir) {
-        this.ocsswDir = ocsswDir;
+    public String getOcsswDataDir(){
+        return ocsswDir + _OCSSW_DATA_DIR_SUFFIX;
+    }
+
+    public String getOcsswScriptsDir(){
+        return ocsswDir + _OCSSW_SCRIPTS_DIR_SUFFIX;
     }
 
 }
