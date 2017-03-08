959f551880c1b22616a30c8c24afdf0c129d604f
knowles <daniel.s.knowles@nasa.gov>
2017-03-08 12:09:29 -0500

    Fixes newly introduce GPT bug due to preferences using VISAT in the seadas file reader.  Added comment in help release notes.

--- seadas-reader/src/main/java/gov/nasa/gsfc/seadas/dataio/SeadasFileReader.java
+++ seadas-reader/src/main/java/gov/nasa/gsfc/seadas/dataio/SeadasFileReader.java
@@ -1550,8 +1550,9 @@ public abstract class SeadasFileReader {
         if (configuration != null) {
             return configuration.getPropertyBool(Mask.ImageType.PARAMETER_NAME_MASK_L2_FLAGNAMES_ENABLED, Mask.ImageType.DEFAULT_L2_FLAGNAMES_ENABLED);
         } else {
-            return true;
+            return Mask.ImageType.DEFAULT_L2_FLAGNAMES_ENABLED;
         }
+
     }
 
 
