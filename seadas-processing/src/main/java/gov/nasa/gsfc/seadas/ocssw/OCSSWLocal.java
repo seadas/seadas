81253c8b345a3f9d40abeee8ea5f1ff1a08cb11d
aynur <aynur.abdurazik@nasa.gov>
2017-09-29 11:27:18 -0400

    Removed the getProcessExitValue() from the ocssw wrapper classes. Moved it to the ProcessObserver class.

--- seadas-processing/src/main/java/gov/nasa/gsfc/seadas/ocssw/OCSSWLocal.java
+++ seadas-processing/src/main/java/gov/nasa/gsfc/seadas/ocssw/OCSSWLocal.java
@@ -90,11 +90,6 @@ public class OCSSWLocal extends OCSSW {
     }
 
     @Override
-    public int getProcessExitValue(Process process) {
-        return process.exitValue();
-    }
-
-    @Override
     public Process execute(ParamList paramListl) {
         String[] programNameArray = {programName};
         commandArray = SeadasArrayUtils.concatAll(commandArrayPrefix, programNameArray, getCommandArrayParam(paramListl), commandArraySuffix);
