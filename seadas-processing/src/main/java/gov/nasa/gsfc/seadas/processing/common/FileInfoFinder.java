17eac387888e57afc513e17d7eee474dfa72fe04
aynur <aynur.abdurazik@nasa.gov>
2017-10-17 10:31:34 -0400

    Code cleanup.

--- seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/common/FileInfoFinder.java
+++ seadas-processing/src/main/java/gov/nasa/gsfc/seadas/processing/common/FileInfoFinder.java
@@ -26,37 +26,12 @@ public class FileInfoFinder {
 
     public FileInfoFinder(String fileName, OCSSW ocssw){
         this.ocssw = ocssw;
-        //ocssw.setIfileName(fileName);
         ocssw.getOfileName(fileName);
         setMissionName(ocssw.getMissionName());
         setFileType(ocssw.getFileType());
         setMissionDirName(OCSSWInfo.getInstance().getOcsswDataDirPath());
-//        fileInfoFinderProcessorModel = new ProcessorModel(FILE_INFO_SYSTEM_CALL, ocssw);
-//        fileInfoFinderProcessorModel.setAcceptsParFile(false);
-//        addParamInfo("file", fileName, ParamInfo.Type.IFILE, ParamInfo.USED_IN_COMMAND_AS_ARGUMENT, 0);
-//        fileInfos = new HashMap();
     }
 
-//    private void addParamInfo(String paramName, String paramValue, ParamInfo.Type paramType, String usedAs, int order){
-//        ParamInfo paramInfo = new ParamInfo(paramName, paramValue, paramType);
-//        paramInfo.setOrder(order);
-//        paramInfo.setUsedAs(usedAs);
-//        fileInfoFinderProcessorModel.addParamInfo(paramInfo);
-//        identifyFileInfo();
-//    }
-//
-//    private void identifyFileInfo(){
-//        if (OCSSWOldModel.isLocal()) {
-//            commandArrayManager = new LocalOcsswCommandArrayManager(fileInfoFinderProcessorModel);
-//            //fileInfos = OCSSWRunnerOld.executeLocalGetOBPGFileInfo(commandArrayManager.getProgramCommandArray(), commandArrayManager.getIfileDir());
-//        } else {
-//            commandArrayManager = new RemoteOcsswCommandArrayManager(fileInfoFinderProcessorModel);
-//            //fileInfos = OCSSWRunnerOld.executeRemoteGetOBPGFileInfo(commandArrayManager.getProgramCommandArray());
-//        }
-//        setFileType(fileInfos.get(FILE_TYPE_ID_STRING));
-//        setMissionName(fileInfos.get(MISSION_NAME_ID_STRING));
-//        setMissionDirName(fileInfos.get(MISSION_DIR_NAME_ID_STRING));
-//    }
     public String getFileType() {
         return fileType;
     }
