94d8d17e380b15a8add156446c6b395b932b4a17
Aynur Abdurazik <aynur.abdurazik@nasa.gov>
2018-01-22 11:28:00 -0500

    Added a new class to monitor the execution of programs invoked without going through the CallCloProgramAction class.

---
+++ seadas-processing/src/main/java/gov/nasa/gsfc/seadas/ocssw/OCSSWExecutionMonitor.java
@@ -0,0 +1,64 @@
+package gov.nasa.gsfc.seadas.ocssw;
+
+import com.bc.ceres.core.ProgressMonitor;
+import com.bc.ceres.swing.progress.ProgressMonitorSwingWorker;
+import gov.nasa.gsfc.seadas.OCSSWInfo;
+import gov.nasa.gsfc.seadas.ocssw.OCSSW;
+import gov.nasa.gsfc.seadas.processing.common.CallCloProgramAction;
+import gov.nasa.gsfc.seadas.processing.common.SeadasFileUtils;
+import gov.nasa.gsfc.seadas.processing.core.ProcessObserver;
+import gov.nasa.gsfc.seadas.processing.core.ProcessorModel;
+import org.esa.beam.visat.VisatApp;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * Created by aabduraz on 1/17/18.
+ */
+public class OCSSWExecutionMonitor {
+
+    public void executeWithProgressMonitor(ProcessorModel processorModel, OCSSW ocssw, String programName){
+
+        VisatApp visatApp = VisatApp.getApp();
+
+        ProgressMonitorSwingWorker swingWorker = new ProgressMonitorSwingWorker<String, Object>(visatApp.getApplicationWindow(), "Running " + programName + " ...") {
+            @Override
+            protected String doInBackground(ProgressMonitor pm) throws Exception {
+
+                ocssw.setMonitorProgress(true);
+                final Process process = ocssw.execute(processorModel.getParamList());//ocssw.execute(processorModel.getParamList()); //OCSSWRunnerOld.execute(processorModel);
+                if (process == null) {
+                    throw new IOException(programName + " failed to create process.");
+                }
+                final ProcessObserver processObserver = ocssw.getOCSSWProcessObserver(process, programName, pm);
+                final CallCloProgramAction.ConsoleHandler ch = new CallCloProgramAction.ConsoleHandler(programName);
+                if (programName.equals(OCSSWInfo.getInstance().OCSSW_INSTALLER_PROGRAM_NAME)) {
+                    processObserver.addHandler(new CallCloProgramAction.InstallerHandler(programName, processorModel.getProgressPattern()));
+                } else {
+                    processObserver.addHandler(new CallCloProgramAction.ProgressHandler(programName, processorModel.getProgressPattern()));
+                }
+                processObserver.addHandler(ch);
+                processObserver.startAndWait();
+                processorModel.setExecutionLogMessage(ch.getExecutionErrorLog());
+
+                int exitCode = processObserver.getProcessExitValue();
+
+                if (exitCode == 0) {
+                    pm.done();
+                    SeadasFileUtils.writeToDisk(processorModel.getIFileDir() + System.getProperty("file.separator") + "OCSSW_LOG_" + programName + ".txt",
+                            "Execution log for " + "\n" + Arrays.toString(ocssw.getCommandArray()) + "\n" + processorModel.getExecutionLogMessage());
+                } else
+                {
+                    throw new IOException(programName + " failed with exit code " + exitCode + ".\nCheck log for more details.");
+                }
+
+
+                ocssw.setMonitorProgress(false);
+                return processorModel.getOfileName();
+            }
+        };
+        swingWorker.execute();
+    }
+}
+
